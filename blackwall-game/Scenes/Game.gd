3242dc7b95bbd4784d24b3cb8a9d928e8a252f00
horw0 <horw0@proton.me>
2025-12-03 01:40:31 +0300

    Chat func
    Playing around

---
+++ blackwall-game/Scenes/Game.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+var users: Dictionary[String, ChatUser]
+
+func _init() -> void:
+	users = {
+		"zd4da": ChatUser.new("zd4da", "red"),
+		"bm00n": ChatUser.new("bm00n", "royal_blue"),
+		"w3iB": ChatUser.new("w3iB", "white"),
+		"v7uga": ChatUser.new("v7uga", "sky_blue")
+	}
+	
+func _ready() -> void:
+	var chat_controller: ChatTerminal = $InnerScreen/ChatController
+	for user: ChatUser in users.values():
+		if chat_controller.hook_user(user):
+			print("User %s connected to %s" % [user.username, chat_controller])
+			user.join_chat()
+	users["zd4da"].write_chat("testing chat, oi there")
